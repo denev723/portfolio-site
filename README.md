4a3ae0e6a965e8688bac382843eec5b03b3faab3
denev <denev723@gmail.com>
2021-07-11 15:45:38 +0900

    SEO 최적화

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Denev's Portfoilo
 리액트를 이용한 포트폴리오 페이지
+
+##
--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import About from "./components/About";
 import Bootcamp from "./components/Bootcamp";
 import Layout from "./components/Layout";
 import Project from "./components/Project";
+import ReactHelmet from "./components/ReactHelmet";
 import Skills from "./components/Skills";
 
 function App() {
   return (
-    <Layout>
-      <Skills />
-      <Bootcamp />
-      <Project />
-      <About />
-    </Layout>
+    <>
+      <ReactHelmet
+        keywords="개발자, 개발자 포트폴리오, 프론트엔드"
+        description="신입 개발자 김병주의 포트폴리오 페이지입니다."
+        title="Denev's Portfolio"
+        favicon="/images/favicon.ico"
+      />
+      <Layout>
+        <Skills />
+        <Bootcamp />
+        <Project />
+        <About />
+      </Layout>
+    </>
   );
 }
 
